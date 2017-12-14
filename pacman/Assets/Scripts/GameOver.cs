37bec5fac02cf16cc588df9cc31fb8d6530e10d7
HaydarTas <haydartas1982@gmail.com>
2017-12-15 00:35:45 +0100

    Har tilføjet alle scener sammen, men de skal bare arbejde sammen. Har også tilføjet en start og Game Over scene

---
+++ pacman/Assets/Scripts/GameOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void OnGUI () {
+		// Retry starter spillet forfra
+		//GUI.contentColor = Color.red;
+		//if (GUI.Button(new Rect(Screen.width/2-50,Screen.height/2 +200,100,40)"Retry?")) {
+			//Application.LoadLevel(0);
+		}
+
+		//Exit button
+		//if (GUI.Button(new Rect(Screen.width/2-50,Screen.height/2 +200,100,40)"Quit?")) {
+		//Application.Quit();
+	//}
+//}
+}
