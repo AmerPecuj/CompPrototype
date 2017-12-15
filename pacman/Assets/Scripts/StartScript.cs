e3b5e6934a4abc6e15625de65941023ae1548dba
HaydarTas <haydartas1982@gmail.com>
2017-12-15 18:10:19 +0100

    Add Quit button

--- pacman/Assets/Scripts/StartScript.cs
+++ pacman/Assets/Scripts/StartScript.cs
@@ -10,15 +10,19 @@ public class StartScript : MonoBehaviour {
 	}
 
 	void OnGUI () {
-		// Retry starter spillet forfra
+
 	GUI.contentColor = Color.yellow;
 	if (GUI.Button(new Rect(Screen.width/2 -40,Screen.height/2 +50,100,30),"Play")) {
 		Application.LoadLevel(1);
 	}
 
-		//Exit button
+
 	if (GUI.Button(new Rect(Screen.width/2 -40,Screen.height/2 +100,100,30),"Settings")) {
 	Application.LoadLevel(5);
+}
+
+	if (GUI.Button(new Rect(Screen.width/2 -40,Screen.height/2 +150,100,30),"Quit")) {
+		Application.Quit();
 }
 
 }
